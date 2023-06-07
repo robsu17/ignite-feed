dc55f5901f4255a2cc5e4ac691ed38846ab0e246
robsu17 <robson.costa134@gmail.com>
2023-06-07 20:45:38 -0300

    sidebar finalized

--- ignite-feed/src/components/Sidebar.jsx
+++ ignite-feed/src/components/Sidebar.jsx
@@ -1,9 +1,26 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
+
+import backgroundImg from '../assets/background-sidebar.svg'
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      Sidebar
+      <img className={styles.cover} src={backgroundImg}/>
+      
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/robsu17.png"/>
+        
+        <strong>Leslie Alexander</strong>
+        <span>Ui Designer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/> Editar seu perfil
+        </a>
+      </footer>
     </aside>
   )
 }
