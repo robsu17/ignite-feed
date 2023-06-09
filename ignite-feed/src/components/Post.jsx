b2ae93527fba0288c8ddc896c5dcb3e81242929f
robsu17 <robson.costa134@gmail.com>
2023-06-09 15:23:43 -0300

    Update Post.jsx

--- ignite-feed/src/components/Post.jsx
+++ ignite-feed/src/components/Post.jsx
@@ -11,10 +11,16 @@ export function Post() {
             <span>Web Developer</span>
           </div>
         </div>
-        <time>
-          
-        </time>
+        <time title='11 de mario às 8:13h' dateTime='2022-05-11 08:13:30'>Publicado há 1 hora</time>
       </header>
+
+      <div className={styles.content}>
+        <p>Fala Galera</p>
+
+        <p>Acabei de subir mais um projeto no portifólio. é um projeto que fiz no NLW Return, evento da rocketseat</p>
+        <p><a href="#">jane.design/doctorcare</a></p>
+        <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+      </div>
     </article>
   )
 }
